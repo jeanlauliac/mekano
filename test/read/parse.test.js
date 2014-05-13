9217f2c1dc62b2a880f06d4946c6fe53a5ef9d64
Jean Lauliac <jean@lauliac.com>
2014-05-13 13:35:26 +0200

    Parse work

---
+++ test/read/parse.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var test = require('tape')
+var StringStream = require('./string-stream')
+var lex = require('../../lib/read/lex')
+var parse = require('../../lib/read/parse')
+
+testParse('recipe', 'Foo: beep boop', function (t, unit) {
+    console.error(unit)
+})
+
+function testParse(name, str, cb) {
+    test('parse() ' + name, function (t) {
+        var ss = new StringStream(str)
+        ss.pipe(lex()).pipe(parse()).on('parsed', function (unit) {
+            cb(t, unit)
+            t.end()
+        })
+    })
+}
