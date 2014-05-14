7d1eff0e2862b6aed161b72248ca8ccf6fd8e1b5
Jean Lauliac <jean@lauliac.com>
2014-05-14 18:03:56 +0200

    Add binds parse

--- test/read/parse.test.js
+++ test/read/parse.test.js
@@ -5,6 +5,8 @@ var StringStream = require('./string-stream')
 var lex = require('../../lib/read/lex')
 var parse = require('../../lib/read/parse')
 
+var TRACE = true
+
 testParse('recipe', 'Foo: `beep`;', function (t, unit) {
     t.equal(unit.recipes.length, 1)
     var r = unit.recipes[0]
@@ -12,10 +14,18 @@ testParse('recipe', 'Foo: `beep`;', function (t, unit) {
     t.equal(r.command, 'beep')
 })
 
+testParse('bind', 'Foo = `beep`;', function (t, unit) {
+    t.equal(unit.binds.length, 1)
+    var b = unit.binds[0]
+    t.equal(b.name, 'Foo')
+    t.equal(b.value, 'beep')
+})
+
 function testParse(name, str, cb) {
     test('parse() ' + name, function (t) {
         var ss = new StringStream(str)
-        ss.pipe(lex()).pipe(parse()).on('parsed', function (unit) {
+        var ps = parse({trace: TRACE})
+        ss.pipe(lex()).pipe(ps).on('parsed', function (unit) {
             cb(t, unit)
             t.end()
         })
