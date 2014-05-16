4c9a881180e89b0cdedf37a59563d4f4513adcbb
Jean Lauliac <jean@lauliac.com>
2014-05-16 15:23:37 +0200

    Fix singlify()

--- test/read/singlify.test.js
+++ test/read/singlify.test.js
@@ -2,32 +2,34 @@
 
 var test = require('tape')
 var singlify = require('../../lib/read/singlify')
-var ast = require('../../lib/read/ast')
+var interRep = require('../../lib/read/inter-rep')
 var Token = require('../../lib/read/token.js')
 var Location = require('../../lib/read/location.js')
 
 var TEST_RELS = [
-    new ast.Relation([
+    new interRep.ExpRelation([
         tokenOf(Token.PATH, 'foo.c')
       , tokenOf(Token.PATH, 'bar.c')
     ], [
-        new ast.Trans('Compile', true, [
+        new interRep.ExpTrans({recipeName: 'Compile'}, [
             tokenOf(Token.PATH, 'foo.o')
           , tokenOf(Token.PATH, 'bar.o')
         ])
-      , new ast.Trans('Link', false, [tokenOf(Token.PATH, 'a.out')])
-    ], new ast.Alias('all'))
+      , new interRep.ExpTrans({recipeName: 'Link'}, [
+            tokenOf(Token.PATH, 'a.out')
+        ])
+    ])
 ]
 
 test('read.singlify() ', function (t) {
-    var res = singlify(TEST_RELS)
-    var transs = res.transs
+    var transs = singlify(TEST_RELS)
     t.equal(transs.length, 2)
     t.equal(transs[0].ast.recipeName, 'Compile')
     t.equal(transs[1].ast.recipeName, 'Link')
     t.equal(transs[0].prereqs[0].value, 'foo.c')
+    t.equal(transs[0].targets[0].value, 'foo.o')
     t.equal(transs[1].prereqs[0].value, 'foo.o')
-    t.equal(res.aliases['all'].ast.name, 'all')
+    t.equal(transs[1].targets[0].value, 'a.out')
     t.end()
 })
 
