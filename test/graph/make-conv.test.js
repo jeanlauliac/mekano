a80ac0844787f38a617fc6b7dcc8efc6188f1d28
Jean Lauliac <jean@lauliac.com>
2014-05-16 19:08:42 +0200

    Add graph.makeConv()

---
+++ test/graph/make-conv.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var test = require('tape')
+var makeConv = require('../../lib/graph/make-conv')
+
+test('graph.makeConv() single star', function (t) {
+    var conv = makeConv('source/*.c', 'build/*.o')
+    t.equal(conv('source/foo.c'), 'build/foo.o')
+    t.equal(conv('source/bar.c'), 'build/bar.o')
+    t.end()
+})
+
+test('graph.makeConv() double star', function (t) {
+    var conv = makeConv('source/**/*.c', 'build/**/*.o')
+    t.equal(conv('source/path/to/foo.c'), 'build/path/to/foo.o')
+    t.end()
+})
