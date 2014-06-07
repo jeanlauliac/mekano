5f2ff3598a59a835fa1a5614291d2daeb3c5353d
Jean Lauliac <jean@lauliac.com>
2014-06-07 11:25:39 +0200

    Add some tests

---
+++ test/forward-events.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var test = require('tape')
+var forwardEvents = require('../lib/forward-events')
+var EventEmitter = require('events').EventEmitter
+
+test('forwardEvents()', function (t) {
+    var to = new EventEmitter()
+    var from = new EventEmitter()
+    forwardEvents(to, from, function (errored, str) {
+        t.equal(errored, false)
+        t.equal(str, 'foobar')
+        t.end()
+    })
+    from.emit('finish', 'foobar')
+})
+
+test('forwardEvents() w/ error', function (t) {
+    var to = new EventEmitter()
+    var from = new EventEmitter()
+    var errors = []
+    var warnings = []
+    forwardEvents(to, from, function (errored, str) {
+        t.equal(errored, true)
+        t.equal(errors[0], 'cake')
+        t.equal(errors[1], 'fizz')
+        t.equal(warnings[0], 'baz')
+        t.equal(str, 'foobar')
+        t.end()
+    })
+    to.on('error', function (err) { errors.push(err) })
+    to.on('warning', function (err) { warnings.push(err) })
+    from.emit('error', 'cake')
+    from.emit('warning', 'baz')
+    from.emit('error', 'fizz')
+    from.emit('finish', 'foobar')
+})
--- test/update/run-edges.test.js
+++ test/update/run-edges.test.js
@@ -10,11 +10,29 @@ var RES = ['foo.o', 'bar.o', 'a.out']
 
 test('update.runEdges()', function (t) {
     var i = 0
-    t.plan(3)
     var runEdge = function (edge, cb) {
         t.equal(edge.outFiles[0].path, RES[i])
         process.nextTick(cb.bind(null, null))
         i++
     }
-    runEdges(TEST_EDGES, runEdge, 4).on('finish', function () {})
+    runEdges(TEST_EDGES, runEdge, 4).on('finish', function () {
+        t.equal(i, 3)
+        t.end()
+    })
+})
+
+test('update.runEdges() w/ error', function (t) {
+    var i = 0
+    var runEdge = function (edge, cb) {
+        t.equal(edge.outFiles[0].path, RES[i])
+        var err = i === 0 ? new Error('some error') : null
+        process.nextTick(cb.bind(null, err))
+        i++
+    }
+    runEdges(TEST_EDGES, runEdge, 4).on('finish', function () {
+        t.equal(i, 2)
+        t.end()
+    }).on('error', function (err) {
+        t.equal(err.message, 'some error')
+    })
 })
