82a223e674dcfbe8ce18fb3732194281979b1230
Jean Lauliac <jean@lauliac.com>
2014-06-25 11:39:12 +0200

    Better ejs template, and new info

--- bin/util/reduce-ejs.js
+++ bin/util/reduce-ejs.js
@@ -2,16 +2,27 @@
 'use strict';
 
 var fs = require('fs')
+var path = require('path')
 var ejs = require('ejs')
 
 main()
 function main() {
     var files = process.argv.slice(2)
-    var res = files.reduce(function forFile(inner, filePath) {
-        var opts = {
-            inner: inner
+    files.forEach(forFile)
+}
+
+function forFile(filePath) {
+    var nextPath = null
+    var opts = {
+        inner: ''
+      , template: function (newPath) {
+            nextPath = path.join(path.dirname(filePath), newPath)
         }
-        return ejs.render(fs.readFileSync(filePath, 'utf8'), opts)
-    }, '')
-    process.stdout.write(res)
+    }
+    while (filePath !== null) {
+        opts.inner = ejs.render(fs.readFileSync(filePath, 'utf8'), opts)
+        filePath = nextPath
+        nextPath = null
+    }
+    process.stdout.write(opts.inner)
 }
